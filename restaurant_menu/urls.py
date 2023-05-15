64cd549b8cbabb6cbc740537e698ad3042b3df79
Malick <malick.hamidou.boureima@gmail.com>
2023-05-15 15:39:09 -0400

    some css

--- restaurant_menu/urls.py
+++ restaurant_menu/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -6,3 +8,5 @@ urlpatterns = [
     path('item/<int:pk>/', views.MenuItemDetail.as_view(), name="menu_item")
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
